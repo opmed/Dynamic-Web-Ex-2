4820a7f844ec0463055af309c42c837c534d6f5a
Ahmed_Elhussiny <ae1561@nyu.edu>
2020-02-26 09:36:06 -0500

    api key

---
+++ src/Pages/Home.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+
+//API Key
+const defaultKey = "f91f83e1b6b389ae69819bdb9b3fb8b7"
+
+
+
+
+function Home() {
+	return (
+		<div>
+		   <h2>Home</h2>
+		</div>
+	);
+};
+
+export default Home;
